e024a1079826f5f635c131da0197c5ce8b3abaa0
Aynissan <aynisamadieva@gmail.com>
2023-01-17 18:43:56 +0600

    auth

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,7 +1,139 @@
 import React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { Link as RouterLink } from "react-router-dom";
+import pic1 from "./assets/Ellipse 8.jpg";
+import { pic2 } from "./assets/Ellipse 9.svg";
+import pic3 from "./assets/Saly-38.svg";
+import "./Login.css";
+// import { ThemeProvider } from "@mui/material/styles";
 
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://makers.kg/">
+        Phyton and JavaScript
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    // <ThemeProvider
+    // theme={theme}
+    // >
+    <div backgroung-image={pic1} className="content">
+      <div backgroung-image={pic3} className="content">
+        <img src={pic1} className="img1" alt="" />
+        <img src={pic3} className="img2" alt="" />
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          ></Box>
+          <RouterLink to="/payvisa">
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+          </RouterLink>
+          {/* <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar> */}
+          <Typography
+            component="h1"
+            variant="h5"
+            color="secondary"
+            fontWeight={700}
+          >
+            WELCOME TO COPP
+          </Typography>
+          <Box
+            component="form"
+            // onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              // value={email}
+              // onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              color="secondary"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              // value={password}
+              // onChange={(e) => setPassword(e.target.value)}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="secondary" />}
+              label="SUBSCRIBE TO FREE"
+            />
+            <Button
+              color="secondary"
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              // onClick={handleSubmit}
+            >
+              Log In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <RouterLink to="/register">
+                  <Typography>{"Don't have an account? Sign Up"}</Typography>
+                </RouterLink>
+              </Grid>
+            </Grid>
+          </Box>
+          <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
+      </div>
+    </div>
+    // </ThemeProvider>
+  );
 };
 
 export default Login;
--- src/MainRoutes.js
+++ src/MainRoutes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./Components/Auth/Login";
 import Register from "./Components/Auth/Register";
+import PayVisa from "./Components/Cart/PayVisa";
 import Home from "./Components/Home";
 
 const MainRoutes = () => {
@@ -10,6 +11,7 @@ const MainRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/payvisa" element={<PayVisa />} />
     </Routes>
   );
 };
