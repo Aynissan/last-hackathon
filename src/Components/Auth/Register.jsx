97e3940bd350f3ba1cea71e7b0533de6c2b4f2e9
Aynissan <aynisamadieva@gmail.com>
2023-01-22 15:24:36 +0600

    .

--- src/Components/Auth/Register.jsx
+++ src/Components/Auth/Register.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -15,7 +15,8 @@ import { Link as RouterLink } from "react-router-dom";
 import pic1 from "./assets/Ellipse 8.jpg";
 import pic3 from "./assets/Saly-38.svg";
 import "./Register.css";
-// import { ThemeProvider } from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { authContext } from "../Contexts/AuthContext";
 
 function Copyright(props) {
   return (
@@ -34,114 +35,166 @@ function Copyright(props) {
     </Typography>
   );
 }
-const Login = () => {
-  return (
-    // <ThemeProvider
-    // theme={theme}
-    // >
-    <div backgroung-image={pic1} className="content">
-      <div backgroung-image={pic3} className="content">
-        <img src={pic1} className="img1" alt="" />
-        <img src={pic3} className="img2" alt="" />
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          ></Box>
-          <RouterLink to="/adminpage">
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-          </RouterLink>
-          <Typography
-            component="h1"
-            variant="h5"
-            color="secondary"
-            fontWeight={700}
-          >
-            WELCOME TO COPP
-          </Typography>
-          <Box
-            component="form"
-            // onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              // value={email}
-              // onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-              color="secondary"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              // value={password}
-              // onChange={(e) => setPassword(e.target.value)}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Confirm Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              // value={password}
-              // onChange={(e) => setPassword(e.target.value)}
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="secondary" />}
-              label="SUBSCRIBE TO FREE"
-            />
-            <Button
-              color="secondary"
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              // onClick={handleSubmit}
-            >
-              Log In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <RouterLink to="/login">
-                  <Typography>{"Have an account? Sign In"}</Typography>
-                </RouterLink>
-              </Grid>
-            </Grid>
-          </Box>
-          <Copyright sx={{ mt: 8, mb: 4 }} />
-        </Container>
-      </div>
-    </div>
-    // </ThemeProvider>
-  );
-};
 
-export default Login;
+const theme = createTheme();
+
+export default function Register() {
+  const { register } = useContext(authContext);
+
+  const [email, setEmail] = React.useState("");
+
+  const [password, setPassword] = React.useState("");
+
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+  const [codeword, setCodeWord] = React.useState("");
+  const [phone_number, setphone_number] = React.useState("");
+
+  function handleSubmit() {
+    if (
+      !email.trim() ||
+      !password.trim() ||
+      !confirmPassword.trim() ||
+      !codeword.trim() ||
+      !phone_number.trim()
+    ) {
+      alert("Заполните поля!");
+      return;
+    }
+
+    let formData = new FormData();
+    formData.append("email", email);
+    formData.append("password", password);
+    formData.append("password_repeat", confirmPassword);
+    formData.append("codeword", codeword);
+    formData.append("phone_number", phone_number);
+    register(formData);
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div backgroung-image={pic1} className="content">
+        <div backgroung-image={pic3} className="content">
+          <img src={pic1} className="img1" alt="" />
+          <img src={pic3} className="img2" alt="" />
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+              sx={{
+                marginTop: 8,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            ></Box>
+            <RouterLink to="/adminpage">
+              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                <LockOutlinedIcon />
+              </Avatar>
+            </RouterLink>
+            <Typography
+              component="h1"
+              variant="h5"
+              color="secondary"
+              fontWeight={700}
+            >
+              WELCOME TO COPP
+            </Typography>
+            <Box
+              component="form"
+              // onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                color="secondary"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Confirm Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="codeword"
+                label="CodeWord"
+                id="codeword"
+                // autoComplete="current-password"
+                value={codeword}
+                onChange={(e) => setCodeWord(e.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="phone_number"
+                label="Phone Number"
+                id="phone_number"
+                // autoComplete="current-password"
+                value={phone_number}
+                onChange={(e) => setphone_number(e.target.value)}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="secondary" />}
+                label="SUBSCRIBE TO FREE"
+              />
+              <Button
+                color="secondary"
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={handleSubmit}
+              >
+                Log In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <RouterLink to="/login">
+                    <Typography>{"Have an account? Sign In"}</Typography>
+                  </RouterLink>
+                </Grid>
+              </Grid>
+            </Box>
+            <Copyright sx={{ mt: 8, mb: 4 }} />
+          </Container>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
